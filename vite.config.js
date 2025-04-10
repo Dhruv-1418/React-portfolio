e4a3417f5900a9be288f908bd4b3c736087fb4b4
Dhruv Vachhani <raj.vachhani1418@gmail.com>
2025-04-10 12:46:37 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
 
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: "/dhruv-vachhani",
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/dhruv-vachhani",
-});
+  plugins: [react()],
+})
